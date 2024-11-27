4c2441c3319b4373692c361218ba154f78a6b7ad
Nikhil Singh <nikhilpramodsingh@gmail.com>
2024-11-27 16:03:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./components/heading";
+import Api from "./components/api";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <Heading />
+      <Api />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/heading.jsx
@@ -0,0 +1,13 @@
+import "../components/style/heading.css";
+
+function Heading() {
+  return (
+    <>
+      <div className="heading">
+        <h1 className=""> Pokemon List</h1>
+      </div>
+    </>
+  );
+}
+
+export default Heading;
